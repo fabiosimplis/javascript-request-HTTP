236329c96ebe04d91c4c16c22b34b3a41109c3c8
fabiosimplis <fabio.simplis@gmail.com>
2025-01-07 17:18:06 -0300

    add constante para URL

--- js/api.js
+++ js/api.js
@@ -1,8 +1,10 @@
+const URL_BASE = 'http://localhost:3000';
+
 const api = {
 
     async buscarPensamentos() {
         try {
-            const response = await fetch ('http://localhost:3000/pensamentos');
+            const response = await fetch (`${URL_BASE}/pensamentos`);
             return await response.json();
         } catch {
             alert('ERROR: Busca de pensamentos');
@@ -12,7 +14,7 @@ const api = {
 
     async salvarPensamentos(pensamento) {
         try {
-            const response = await fetch ('http://localhost:3000/pensamentos', {
+            const response = await fetch (`${URL_BASE}/pensamentos`, {
                 method: "POST",
                 headers: {
                     "Content-type": "application/json"
@@ -28,7 +30,7 @@ const api = {
 
     async buscarPensamentoPorId(id) {
         try {
-            const response = await fetch (`http://localhost:3000/pensamentos/${id}`);
+            const response = await fetch (`${URL_BASE}/pensamentos/${id}`);
             return await response.json();
         } catch {
             alert(`ERROR: Busca de pensamento de id: ${id}`);
@@ -38,7 +40,7 @@ const api = {
 
     async editarPensamento(pensamento) {
         try {
-            const response = await fetch (`http://localhost:3000/pensamentos/${pensamento.id}`, {
+            const response = await fetch (`${URL_BASE}/pensamentos/${pensamento.id}`, {
                 method: "PUT",
                 headers: {
                     "Content-type": "application/json"
@@ -54,7 +56,7 @@ const api = {
 
     async excluirPensamento(id) {
         try {
-            const response = await fetch (`http://localhost:3000/pensamentos/${id}`, {
+            const response = await fetch (`${URL_BASE}/pensamentos/${id}`, {
                 method: "DELETE",
             });
         } catch {
