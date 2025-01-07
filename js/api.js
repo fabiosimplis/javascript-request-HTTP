672a882dc7670774c05172b08237b2713b8fb8be
fabiosimplis <fabio.simplis@gmail.com>
2025-01-07 11:49:45 -0300

    add dados com POST

--- js/api.js
+++ js/api.js
@@ -8,6 +8,22 @@ const api = {
             alert('ERROR: Busca de pensamentos');
             throw error;
         }
+    },
+
+    async salvarPensamentos(pensamento) {
+        try {
+            const response = await fetch ('http://localhost:3000/pensamentos', {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            });
+            return await response.json();
+        } catch {
+            alert('ERROR: Busca de pensamentos');
+            throw error;
+        }
     }
 }
 
